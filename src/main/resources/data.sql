06e6d2e921b3bb25c048598bdf4145eaedd7c660
zhonghao <593513235@qq.com>
2019-08-18 16:18:25 +0800

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+
+insert into user (email, name, password, username) values ('5935313235@qq.com','hanzhonghao','1','h5ao');
+insert into user (email, name, password, username) values ('593531r3235@qq.com','hanzhonghao','1','h4ao');
+insert into user (email, name, password, username) values ('593531r53235@qq.com','hanzhonghao','1','thao');
+insert into user (email, name, password, username) values ('593531r356235@qq.com','hanzhonghao','1','hyao');
+insert into user (email, name, password, username) values ('59353132bf35@qq.com','hanzhonghao','1','hrao');
+insert into user (email, name, password, username) values ('59353ev3235@qq.com','hanzhonghao','1','haevo');
+insert into user (email, name, password, username) values ('593531ev3235@qq.com','hanzhonghao','1','hatro');
+insert into user (email, name, password, username) values ('59353132tvr35@qq.com','hanzhonghao','1','hvtrao');
+insert into user (email, name, password, username) values ('59354313vr235@qq.com','hanzhonghao','1','hrtyuhvao');
+insert into user (email, name, password, username) values ('59353r13vr235@qq.com','hanzhonghao','1','hryuvao');
+insert into user (email, name, password, username) values ('59353e13vr235@qq.com','hanzhonghao','1','hrvnyao');
+insert into user (email, name, password, username) values ('593531t3vr235@qq.com','hanzhonghao','1','hrtyrvao');
+insert into user (email, name, password, username) values ('5935315y3vr235@qq.com','hanzhonghao','1','hegrvao');
+insert into user (email, name, password, username) values ('59353ty5g13vr235@qq.com','hanzhonghao','1','hrtvao');
+insert into user (email, name, password, username) values ('5935tr313vr235@qq.com','hanzhonghao','1','hrvtyao');
+insert into user (email, name, password, username) values ('593531tr3vr235@qq.com','hanzhonghao','1','hrvfrgao');
